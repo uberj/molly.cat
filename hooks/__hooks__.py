3f76551f250b8966c02c50a9e04d4a0094e8101a
uberj <uberj@onid.orst.edu>
2013-09-20 09:09:58 -0700

    use sriracha hooks

--- hooks/__hooks__.py
+++ hooks/__hooks__.py
@@ -1,11 +1,11 @@
-from hooks import compile_sass
-
+import thumbnail
 from gallery import Gallery
+
 
 gallery = Gallery()
 
 hooks = {
+    'site.start': [thumbnail.create_thumbnails],
     'page.meta.post': [gallery.get_images],
     'page.template.pre': [gallery.get_albums, gallery.set_images],
-    'site.output.post': [compile_sass],
 }
---
+++ hooks/config.py
@@ -0,0 +1,3 @@
+GALLERY_WOK_TYPE = 'index'  # Must match type in content/gallery.mkd.
+IMGUR_CLIENT_ID = 'f151f673c7118b9'
+FLICKR_CLIENT_ID = '46c333ffa7c04c5a23d7b9c19d734216'
